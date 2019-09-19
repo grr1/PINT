875cd1af29942049d842e3251b5e98bd91efe450
viswapurdue <balajiv@purdue.edu>
2019-09-18 21:34:22 -0400

    basic classes

---
+++ Node.cc
@@ -0,0 +1,6 @@
+#include "Node.h"
+
+unsigned int Node::getNodeID()
+{
+  return node_id;
+}
---
+++ graph.cc
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include "Node.h"
+using namespace std;
+
+class Graph
+{
+private:
+  vector<Node> nodes;
+  int node_count;
+
+public:
+  void addNode()
+  {
+    nodes.push_back();
+  }
+};
+
+int main()
+{
+  return 0;
+}
