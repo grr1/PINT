b5a91f02d7cf3295b8d6c654ee5a7dd09cf6fc10
viswapurdue <balajiv@purdue.edu>
2019-09-19 18:35:34 -0400

    heap corrected

--- graph.cc
+++ graph.cc
@@ -11,25 +11,24 @@ private:
   map<string, Node *> nodes;
 
 public:
-  Graph addNode(string node_id, string left_parent_id = "", string right_parent_id = "", double (*func)(double, double) = NULL)
+  void addNode(string node_id, string left_parent_id = "", string right_parent_id = "", double (*func)(double, double) = NULL)
   {
     Node *left_parent  = nodes.count(left_parent_id)>0 ? nodes[left_parent_id]: NULL;
     Node *right_parent = nodes.count(right_parent_id)>0 ? nodes[right_parent_id]: NULL;
-    Node node(node_id,left_parent, right_parent, func);
-    nodes.insert(pair<string, Node *>(node_id, &node));
-    return *this;
+    Node *node = new Node(node_id,left_parent, right_parent, func);
+    this->nodes.insert(pair<string, Node *>(node_id, node));
   }
 
-  Graph setNode(string node_id, double value)
+  void setNode(string node_id, double value)
   {
-    cout<<nodes[node_id]->node_id;
-    nodes[node_id]->result = value;
-    return *this;
+    cout<<this->nodes.count(node_id)<<endl;
+    cout<<this->nodes[node_id]->node_id;
+    this->nodes[node_id]->result = value;
   }
 
   double evalNode(string node_id)
   {
-    Node *node = nodes[node_id];
+    Node *node = this->nodes[node_id];
     cout << node->node_id;
    return 0;// (node->operation)(node->left_parent->result, node->right_parent->result);
   }
