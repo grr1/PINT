6e5cf55babea147034cacda7d84b48bc38537bc7
Anthony Paul Niemiec <aniemie@data.cs.purdue.edu>
2019-09-20 01:05:43 -0400

    making directory structore more reminiscent of XINU's file structure

---
+++ include/node.h
@@ -0,0 +1,24 @@
+#include <string>
+using namespace std;
+
+class Node
+{
+private:
+  string node_id;
+  Node* left_parent;
+  Node* right_parent;
+  double (*operation)(double, double);
+  void *derivate;
+  double result;
+  bool pin;
+
+  double compute();
+
+public:
+  Node(string, Node*, Node*, double (*func)(double, double));
+  void setPin(bool);
+  bool getPin();
+  void setResult(double);
+  double getResult();
+
+};
