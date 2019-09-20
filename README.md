e9f56095575918f712e71d807948f571d51fd1fd
Anthony Paul Niemiec <aniemie@data.cs.purdue.edu>
2019-09-20 01:16:54 -0400

    creating remote branch

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # PINT
-PINT Is Not Tensorflow
-Run the code using the following command, with the files in the same directory
-g++ -o pint graph.cc node.c
-./pint
+PINT Is Not Tensorflow\
+Run the code using the following command, with the files in the same directory\
+g++ -o pint graph.cc node.c\
+./pint\
 
 #Authors
-Viswajeeet Balaji
-Sri Cherukuri
+Viswajeeet Balaji\
+Sri Cherukuri\
+Anthony Niemiec
---
+++ dataflowGraph/node.cc
@@ -0,0 +1,43 @@
+#include <pint.h>
+
+Node::Node(string node_id, Node* left, Node* right, double (*func)(double, double))
+{
+  this->node_id = node_id;
+  this->left_parent = left;
+  this->right_parent = right;
+  this->operation = func;
+  this->pin = false;
+}
+
+double Node::compute()
+{
+  return (this->operation)(this->left_parent->getResult(), this->right_parent->getResult());
+}
+
+double Node::getResult()
+{
+  if(this->getPin())
+  {
+    return this->result;
+  }
+  else
+  {
+    return this->compute();
+  }
+}
+
+void Node::setResult(double value)
+{
+  this->result = value;
+  this->setPin(true);
+}
+
+bool Node::getPin()
+{
+  return this->pin;
+}
+
+void Node::setPin(bool val)
+{
+  this->pin = true;
+}
