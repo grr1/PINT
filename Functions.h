c96470c52612791bf92a4d04cb89837d750d2a10
viswapurdue <balajiv@purdue.edu>
2019-09-20 14:12:17 -0400

    functions.h

---
+++ Functions.h
@@ -0,0 +1,20 @@
+
+double add(double a, double b)
+{
+  return a+b;
+}
+
+double mult(double a, double b)
+{
+  return a*b;
+}
+
+double div(double a, double b)
+{
+  return a/b;
+}
+
+double sub(double a, double b)
+{
+  return a-b;
+}
