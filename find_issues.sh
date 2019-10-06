fdbda3331900aea2639f8976db1b2a5e4c0475d8
sri-cherukuri <srivikas99@gmail.com>
2019-10-06 00:01:58 -0400

    Added QOL tags to todo command

--- find_issues.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# returns every filename with an unaddressed "TODO" tag (not case sensitive); ignores this file itself
-grep -irl "TODO" | grep -v `basename "$0"`
-
-# to be fancy, when grep returns no files
-if [ "$?" -eq 1 ] ; then
-	echo "-No files need attention."
-fi
