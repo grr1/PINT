45085977247eb3d58c200ad68b4a27aac87d87b6
sri-cherukuri <srivikas99@gmail.com>
2019-11-08 00:49:26 -0500

    Finished matrix prod and rand init in ptensor

--- include/ptensor.h
+++ include/ptensor.h
@@ -18,15 +18,16 @@ public:
     int _shape[3];
         // shape[0] = nrows = column_size
         // shape[1] = ncols = row_size
-        // shape[2] = nplanes = z_size
+        // shape[2] = nplanes = z_size (ncolors)
 
-    inline double & at(int i, int j, int k=0) { return _data[k*_shape[2]*_shape[1] + j*_shape[1] + i]; }
-    inline double getElement(int i, int j, int k=0) { return _data[k*_shape[2]*_shape[1] + j*_shape[1] + i]; }
-    inline void setElement(double x, int i, int j, int k=0) { _data[k*_shape[2]*_shape[1] + j*_shape[1] + i] = x; }
+    // at(1, 2, 3) means term #1, vector #2, color #3
+    inline double & at(int i=0, int j=0, int k=0) { return _data[k*_shape[1]*_shape[0] + j*_shape[0] + i]; }
+    inline double getElement(int i=0, int j=0, int k=0) { return _data[k*_shape[1]*_shape[0] + j*_shape[0] + i]; }
+    inline void setElement(double x=0, int i=0, int j=0, int k=0) { _data[k*_shape[1]*_shape[0] + j*_shape[0] + i] = x; }
 
     PTensor();
     //PTensor(int ndim, int * shape);
-    PTensor(int ndim, const int * shape, double init=0);
+    PTensor(int ndim, const int* shape);
     PTensor(const PTensor&); // copy constructor
     ~PTensor();
 
@@ -37,27 +38,29 @@ public:
     PTensor & operator*=(const PTensor&);
     PTensor & operator/=(const PTensor&);
 
-    PTensor & operator=(const double &rhs) { return *this += PTensor(this->_ndim, this->_shape, rhs); }
+/*    PTensor & operator=(const double &rhs) { return *this += PTensor(this->_ndim, this->_shape, rhs); }
     PTensor & operator+=(const double &rhs) { return *this += PTensor(this->_ndim, this->_shape, rhs); }
     PTensor & operator-=(const double &rhs) { return *this += PTensor(this->_ndim, this->_shape, rhs); }
     PTensor & operator*=(const double &rhs) { return *this += PTensor(this->_ndim, this->_shape, rhs); }
     PTensor & operator/=(const double &rhs) { return *this += PTensor(this->_ndim, this->_shape, rhs); }
-
+*/
     // Binary arithmetic ops
     const PTensor operator+(const PTensor &other) const { return PTensor(*this) += other; }
     const PTensor operator-(const PTensor &other) const { return PTensor(*this) -= other; }
     const PTensor operator*(const PTensor &other) const { return PTensor(*this) *= other; }
     const PTensor operator/(const PTensor &other) const { return PTensor(*this) /= other; }
 
-    const PTensor operator+(const double &other) const { return PTensor(*this) += other; }
+/*    const PTensor operator+(const double &other) const { return PTensor(*this) += other; }
     const PTensor operator-(const double &other) const { return PTensor(*this) -= other; }
     const PTensor operator*(const double &other) const { return PTensor(*this) *= other; }
     const PTensor operator/(const double &other) const { return PTensor(*this) /= other; }
-
+*/
 
     // Comparison ops
     bool operator==(const PTensor &) const;
     bool operator!=(const PTensor &rhs) const { return !(*this == rhs); }
+
+    // Auxiliary ops
 
     // at end because data is a flexible array member
     double * _data;
@@ -65,7 +68,8 @@ public:
         // matrix[i][j][k] = data[i*shape[0]*shape[1] + j*shape[1] + k]
 };
 
-const PTensor dot(const PTensor&, const PTensor&);
+const PTensor dot(PTensor, PTensor);
+PTensor ptrand(int ndim, const int* shape);
 
 }
 
