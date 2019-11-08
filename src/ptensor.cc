c30bf680122031aad69c56edd87bee49a46da667
sri-cherukuri <srivikas99@gmail.com>
2019-11-08 11:41:51 -0500

    Setup main

--- src/ptensor.cc
+++ src/ptensor.cc
@@ -10,8 +10,8 @@ PTensor::PTensor()
 {
     _ndim = 1;
     _shape[0] = _shape[1] = _shape[2] = 1;
-    _size = sizeof(double);
-    _data = (double *)malloc(_size);
+    _size = 1;
+    _data = (double *)malloc(sizeof(double)*_size);
 }
 
 /*PTensor::PTensor(int ndim, int * shape)
@@ -33,14 +33,13 @@ PTensor::PTensor(int ndim, const int* shape)
     _ndim = min(ndim, 3);
     _shape[0] = _shape[1] = _shape[2] = 1;
 
-    _size = sizeof(double);
+    _size = 1;
     for (int i = 0; i < _ndim; i++)
     {
         _shape[i] = shape[i];
-        _size *= shape[i];
+        _size *= _shape[i];
     }
-
-    _data = (double *)malloc(_size);
+    _data = (double *)malloc(sizeof(double)*_size);
 }
 
 PTensor::PTensor(const PTensor& t)
@@ -53,8 +52,8 @@ PTensor::PTensor(const PTensor& t)
         this->_shape[i] = t._shape[i];
     }
 
-    this->_data = (double *)malloc(this->_size);
-    std::memcpy(this->_data, t._data, this->_size);
+    this->_data = (double *)malloc(sizeof(double)*this->_size);
+    std::memcpy(this->_data, t._data, sizeof(double)*this->_size);
 }
 
 PTensor::~PTensor()
@@ -84,8 +83,8 @@ PTensor & PTensor::operator=(const PTensor &rhs)
     }
 
     free(this->_data);
-    _data = (double *)malloc(this->_size);
-    memcpy(this->_data, rhs._data, this->_size);
+    _data = (double *)malloc(sizeof(double)*this->_size);
+    memcpy(this->_data, rhs._data, sizeof(double)*this->_size);
     return *this;
 }
 
@@ -150,7 +149,7 @@ PTensor & PTensor::operator=(const double &rhs)
 }
 
 // double div
-const PTensor operator/(const double &lhs, const PTensor &rhs)
+const PTensor pint::operator/(const double &lhs, const PTensor &rhs)
 {
     PTensor p(rhs);
 
@@ -176,32 +175,32 @@ bool PTensor::operator==(const PTensor &rhs) const
 }
 
 // Exp
-const PTensor exp(const PTensor a)
+const PTensor pint::exp(const PTensor a)
 {
     PTensor b(a);
-    for (int i = 0; i < a._size; i++) { b._data[i] = exp(a._data[i]); }
+    for (int i = 0; i < a._size; i++) { b._data[i] = std::exp(a._data[i]); }
 
     return b;
 }
 // Dot prod
-const PTensor mult(const PTensor a, const PTensor x)
+const PTensor pint::mult(const PTensor a, const PTensor x)
 {
-    if (a.shape[1] != x.shape[0])
+    if (a._shape[1] != x._shape[0])
     {
         printf("Incompatible matrix dimensions\n");
         exit(1);
     }
     
-    if (a._shape[2] != x.shape[2])
+    if (a._shape[2] != x._shape[2])
     {
         printf("Unequal matrix planes\n");
         exit(1);
     }
 
     int b_shape[3];
-    b_shape[0] = a.shape[0];
-    b_shape[1] = x.shape[1];
-    b_shape[2] = a.shape[2];
+    b_shape[0] = a._shape[0];
+    b_shape[1] = x._shape[1];
+    b_shape[2] = a._shape[2];
 
     PTensor b(a._ndim, b_shape);
 
@@ -214,7 +213,7 @@ const PTensor mult(const PTensor a, const PTensor x)
                 int dot = 0;
                 for (int h = 0; h < a._shape[1]; h++)
                 {
-                    dot += a.at(i,h,,k)*b.at(h,j,k);
+                    dot += a.at(i,h,k)*b.at(h,j,k);
                 }
                 b.at(i,j,k) = dot;
             }
@@ -225,7 +224,7 @@ const PTensor mult(const PTensor a, const PTensor x)
 }
 
 // Rand init
-const PTensor randpt(int ndim, const int* shape)
+const PTensor pint::randpt(int ndim, const int* shape)
 {
     PTensor p(ndim, shape);
 
--- src/sequential_neural_net.cc
+++ src/sequential_neural_net.cc
@@ -8,9 +8,9 @@ namespace pint
   SequentialNet::SequentialNet()
   {
     this->numLayers = 0;
-    this->in = new ReflexvityNode();
+    this->in = new ReflexivityNode();
     this->out = this->in;
-    this->weights = new std::vector<PTensor*>()
+    this->weights = new std::vector<PTensor*>();
   }
 
   SequentialNet::~SequentialNet()
@@ -23,19 +23,19 @@ namespace pint
     //TODO: Add a new randomized weight ptensor of shape inSize x outSize.
     std::vector<OpNode*> parentList;
     parentList.push_back(this->out);
-    OpNode * weightNode = new ReflexvityNode();
+    ReflexivityNode * weightNode = new ReflexivityNode();
     parentList.push_back(weightNode);   //TODO: Tie this to the layer's weight ptensor.
-    OpNode * dotNode = new DotProductNode(parentList);   //NOTE: This is assuming that the first arg of the new version is a parent vector.
+    DotProductNode * dotNode = new DotProductNode(parentList);   //NOTE: This is assuming that the first arg of the new version is a parent vector.
     parentList.clear();
     parentList.push_back(dotNode);
-    OpNode * sigNode = new SigmoidNode(parentList);   //TODO: Add the sigmoid node.
+    SigmoidNode * sigNode = new SigmoidNode(parentList);   //TODO: Add the sigmoid node.
     this->out = sigNode;
     this->numLayers++;
   }
 
-  PTensor * SequentialNet::run(PTensor * input)
+  PTensor SequentialNet::run(PTensor * input)
   {
-    this->in->setTensor(input);
+    ((ReflexivityNode*)(this->in))->setTensor(input); // TODO: is this how to properly maintain a vector of abstracts?
     return this->out->eval();   //NOTE: I assume this is how it's supposed to work, but please check.
   }
 
