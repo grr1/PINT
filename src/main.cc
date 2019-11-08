60e59f035083ddb5475feea29744da263d6235fb
Maxwell J Jacobson <arcosin@users.noreply.github.com>
2019-11-08 03:38:54 -0500

    Changed NeuralNet to SequentialNet

--- src/main.cc
+++ src/main.cc
@@ -7,10 +7,10 @@ int main()
 {
     if (pint::init()) { exit(1); }
     const int input[] = {3,5};
-    PTensor *input = new PTensor(2, input);
-    NeuralNetwork *net = new NeuralNetwork();
+    PTensor * input = new PTensor(2, input);
+    SequentialNet * net = new SequentialNet();
     net->addLayer(2,3);
     net->addLayer(3,1);
-    PTensor *output = net.run(input);
+    PTensor * output = net.run(input);
     return 0;
 }
